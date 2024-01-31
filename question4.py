297a576d6ef355b086fd85eb659819d67f44a21d
Giulio Martini <giulio.martini@getrightdata.com>
2024-01-30 23:47:23 -0500

    4

--- question4.py
+++ question4.py
@@ -1,36 +1,23 @@
-n = 'nominal'
-o = 'ordinal'
-i = 'interval'
-r = 'ratio'
+def question4_a():
+    return ['nominal', 'ordinal']
 
-def question4_1():
-    answer = [n,n]
-    return answer
+def question4_b():
+    return ['ratio', 'ordinal']
 
-def question4_2():
-    answer = [r,o]
-    return answer
+def question4_c():
+    return ['ratio', 'ordinal']
 
-def question4_3():
-    answer = [r,o]
-    return answer 
+def question4_d():
+    return ['ratio', 'ordinal']
 
-def question4_4():
-    answer = [r,o]
-    return answer
+def question4_e():
+    return ['ratio', 'ratio']
 
-def question4_5():
-    answer = [r,r]
-    return answer
+def question4_f():
+    return ['ratio', 'ordinal']
 
-def question4_6():
-    answer = [r,o]
-    return answer
+def question4_g():
+    return ['ratio', 'ratio']
 
-def question4_7():
-    answer = [r,i]
-    return answer
-
-def question4_8():
-    answer = [r,i]
-    return answer
+def question4_h():
+    return ['ratio', 'interval']
