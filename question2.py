f717c26ceb9abd196861d7c2b09311cef08c7a45
Giulio Martini <giulio.martini@getrightdata.com>
2024-01-31 00:13:04 -0500

    hail mary

--- question2.py
+++ question2.py
@@ -1,19 +1,19 @@
 def question2_1():
-    answer = ['discrete', 'quantitative']
+    answer = ['discrete', 'quantitative', 'ratio']
     return answer
 
 def question2_2():
-    answer = ['continuous', 'quantitative']
+    answer = ['continuous', 'quantitative', 'ratio']
     return answer
 
 def question2_3():
-    answer = ['continuous','quantitative']
+    answer = ['continuous','quantitative', 'interval']
     return answer 
 
 def question2_4():
-    answer = ['continuous', 'qualitative']
+    answer = ['continuous', 'qualitative', 'ordinal']
     return answer
 
 def question2_5():
-    answer = ['discrete', 'qualitative']
+    answer = ['discrete', 'qualitative', 'nominal']
     return answer
