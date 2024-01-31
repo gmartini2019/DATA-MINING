ca337d36cfcaabc0f710f549f8d76702a282d141
Giulio Martini <giulio.martini@getrightdata.com>
2024-01-30 23:38:36 -0500

    better2

--- question2.py
+++ question2.py
@@ -11,7 +11,7 @@ def question2_3():
     return answer 
 
 def question2_4():
-    answer = ['discrete', 'quantitative']
+    answer = ['discrete', 'qualitative']
     return answer
 
 def question2_5():
