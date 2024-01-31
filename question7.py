7d98c6e13b27fc4e531d12470488547865af809c
Giulio Martini <giulio.martini@getrightdata.com>
2024-01-30 23:37:30 -0500

    better 7

--- question7.py
+++ question7.py
@@ -11,6 +11,6 @@ def question7_2():
     return answer
 
 def question7_3():
-    answer = [(0.0, 2.0), (2.0, 4.0), (4.0, 6.0), (6.0, 'infinity')]
+    answer = [(0.0, 3.0), (3.0, 4.0), (4.0, 5.0), (5.0, 'infinity')]
     return answer
 
