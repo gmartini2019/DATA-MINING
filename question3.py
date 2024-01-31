26948d9af557ec12948798cd4af06e05d1d8de42
Giulio Martini <giulio.martini@getrightdata.com>
2024-01-30 23:44:51 -0500

    better question3

--- question3.py
+++ question3.py
@@ -8,33 +8,34 @@ r = 'ratio'
 n = 'nominal'
 
 def question3_1():
-    answer = [c,ql, r]
+    answer = [c, qn, r]
     return answer
 
 def question3_2():
-    answer = [d,ql,o]
+    answer = [d, qn, o]
     return answer
 
 def question3_3():
-    answer = [d,ql,n]
+    answer = [d, ql, n]
     return answer 
 
 def question3_4():
-    answer = [c,qn,r]
+    answer = [c, qn, r]
     return answer
 
 def question3_5():
-    answer = [d,ql,n]
+    answer = [d, ql, n]
     return answer
 
 def question3_6():
-    answer = [c,ql,o]
+    answer = [c, qn, r] 
     return answer
 
 def question3_7():
-    answer = [c,ql,o]
+    answer = [d, qn, o] 
     return answer
 
 def question3_8():
-    answer = [d,ql,o]
+    answer = [d, ql, o]  
     return answer
+
